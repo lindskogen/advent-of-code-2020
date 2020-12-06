2eabfd889296db658ecade4598f474800440e3b6
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-12-06 06:52:52 +0100

    Solve day 6

---
+++ src/main/kotlin/day06/main.kt
@@ -0,0 +1,46 @@
+package day06
+
+import day04.chomp
+import java.io.File
+import kotlin.time.measureTimedValue
+
+fun countPart1(string: String): Int {
+    val set = mutableSetOf<Char>()
+
+    for (s in string) {
+        if (s != '\n') {
+            set.add(s)
+        }
+    }
+    return set.count()
+}
+
+fun countPart2(string: String): Int {
+    val map = mutableMapOf<Char, Int>()
+
+    val people = string.split("\n").count()
+
+    for (s in string) {
+        if (s != '\n') {
+            map.compute(s) { _, c -> (c ?: 0) + 1 }
+        }
+    }
+
+    return map.filterValues { it == people }.count()
+}
+
+
+@kotlin.time.ExperimentalTime
+fun main(args: Array<String>) {
+    val (value, elapsed) = measureTimedValue {
+        val questions = File("src/main/kotlin/day06/input").readText().chomp()
+            .split("\n\n")
+        solve(questions)
+    }
+    println(elapsed)
+}
+
+private fun solve(passports: List<String>) {
+    println("Part 1: ${passports.sumOf { countPart1(it) }}")
+    println("Part 2: ${passports.sumOf { countPart2(it) }}")
+}

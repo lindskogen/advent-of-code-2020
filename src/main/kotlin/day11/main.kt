9b8b0ca8efb709c1f8a769ba89ddb83c527ee6c2
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-12-11 07:49:00 +0100

    Day 11

---
+++ src/main/kotlin/day11/main.kt
@@ -0,0 +1,87 @@
+package day11
+
+import java.io.File
+import kotlin.time.measureTimedValue
+
+
+@kotlin.time.ExperimentalTime
+fun main(args: Array<String>) {
+    val (value, elapsed) = measureTimedValue {
+        val lines = File("src/main/kotlin/day11/input").readLines().map { it.toCharArray() }
+
+        val p1 = solve(lines.map { it.clone() }, 4, 1)
+        println("Part 1: $p1")
+        val p2 = solve(lines, 5, Int.MAX_VALUE)
+        println("Part 2: $p2")
+    }
+    println(elapsed)
+}
+
+fun solve(lines: List<CharArray>, numTolerate: Int, maxVision: Int): Int {
+    var changes: Int
+
+    do {
+        changes = 0
+        val oldLines = lines.map { it.clone() }
+
+        for (line in lines.indices) {
+            for (seat in lines[line].indices) {
+                when (oldLines[line][seat]) {
+                    'L' -> {
+                        if (countOccupied(oldLines, line, seat, maxVision) == 0) {
+                            lines[line][seat] = '#';
+                            changes++
+                        }
+                    }
+                    '#' -> {
+                        if (countOccupied(oldLines, line, seat, maxVision) >= numTolerate) {
+                            lines[line][seat] = 'L';
+                            changes++
+                        }
+                    }
+                }
+            }
+        }
+    } while (changes > 0)
+
+    return lines.sumOf { it.count { c -> c == '#' } }
+}
+
+
+private fun countOccupied(
+    lines: List<CharArray>,
+    line: Int,
+    seat: Int,
+    maxVision: Int
+): Int {
+    var count = 0
+
+    for (dl in -1..1) {
+        for (ds in -1..1) {
+            if (dl == 0 && ds == 0) {
+                continue
+            }
+
+            for (step in 1..maxVision) {
+                val l = line + (dl * step)
+                val s = seat + (ds * step)
+
+                if (l !in lines.indices || s !in lines[0].indices) {
+                    break
+                }
+
+                when (lines[l][s]) {
+                    'L' -> {
+                        break
+                    }
+                    '#' -> {
+                        count++
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    return count
+}

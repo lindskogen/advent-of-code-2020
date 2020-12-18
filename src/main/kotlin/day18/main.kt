bd8562bf1b9d113c0503e7ee7d20ec18a9789c69
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-12-18 08:55:45 +0100

    Use parsing tokens

--- src/main/kotlin/day18/main.kt
+++ src/main/kotlin/day18/main.kt
@@ -1,8 +1,8 @@
 package day18
 
 import java.io.File
+import java.lang.Exception
 import java.util.*
-import kotlin.test.assertEquals
 import kotlin.time.measureTimedValue
 
 @kotlin.time.ExperimentalTime
@@ -16,28 +16,26 @@ fun main(args: Array<String>) {
     println(elapsed)
 }
 
-fun hasHigherPrecedence(x: Char, y: Char): Boolean {
-    return x != y && x == '+'
-}
+fun hasHigherPrecedence(x: Char, y: Char): Boolean = x != y && x == '+'
 
 
-fun parse(line: String, isPart2: Boolean): List<String> {
+fun parse(line: String, isPart2: Boolean): List<Token> {
     var i = 0
     val line = line.replace(" ", "")
-    val list = mutableListOf<String>()
+    val list = mutableListOf<Token>()
     val stack = Stack<Char>()
 
     while (i < line.length) {
         val c = line[i]
         when {
             Character.isDigit(c) -> {
-                list.add(c.toString())
+                list.add(Token.Value(c.toString().toLong()))
             }
             c == '*' || c == '+' -> {
                 while (stack.isNotEmpty()
                     && (!isPart2 || hasHigherPrecedence(stack.peek(), c))
                     && stack.peek() != '(') {
-                    list.add(stack.pop().toString())
+                    list.add(operatorCharToToken(stack.pop()))
                 }
                 stack.push(c)
             }
@@ -46,7 +44,7 @@ fun parse(line: String, isPart2: Boolean): List<String> {
             }
             c == ')' -> {
                 while (stack.isNotEmpty() && stack.peek() != '(') {
-                    list.add(stack.pop().toString())
+                    list.add(operatorCharToToken(stack.pop()))
                 }
                 if (stack.isNotEmpty() && stack.peek() == '(') {
                     stack.pop()
@@ -57,36 +55,51 @@ fun parse(line: String, isPart2: Boolean): List<String> {
     }
 
     while (stack.isNotEmpty()) {
-        list.add(stack.pop().toString())
+        list.add(operatorCharToToken(stack.pop()))
     }
 
     return list
 }
 
-fun eval(list: List<String>): Long {
+private fun operatorCharToToken(c: Char) = when (c) {
+    '+' -> Token.Add()
+    '*' -> Token.Multiply()
+    else -> throw Exception("No operator implemented for $c")
+}
+
+open class Token private constructor() {
+    class Value(val num: Long): Token()
+    class Add: Token()
+    class Multiply: Token()
+}
+
+
+
+fun eval(list: List<Token>): Long {
     val list = list.toMutableList()
-    val stack = Stack<String>()
+    val stack = Stack<Token>()
 
     while (list.isNotEmpty()) {
-        val c = list.removeFirst()
-        when {
-            Character.isDigit(c[0]) -> {
-                stack.push(c)
+        when (val t = list.removeFirst()) {
+            is Token.Value -> {
+                stack.push(t)
             }
-            c[0] == '*' -> {
-                val o1 = stack.pop().toString().toLong()
-                val o2 = stack.pop().toString().toLong()
-                stack.push((o1 * o2).toString())
+            is Token.Multiply -> {
+                val o1 = stack.pop() as Token.Value
+                val o2 = stack.pop() as Token.Value
+                stack.push(Token.Value(o1.num * o2.num))
             }
-            c[0] == '+' -> {
-                val o1 = stack.pop().toString().toLong()
-                val o2 = stack.pop().toString().toLong()
-                stack.push((o1 + o2).toString())
+            is Token.Add -> {
+                val o1 = stack.pop() as Token.Value
+                val o2 = stack.pop() as Token.Value
+
+                stack.push(Token.Value(o1.num + o2.num))
             }
         }
     }
 
-    return stack.pop().toString().toLong()
+    val token = stack.pop() as Token.Value
+    return token.num
 }
 
 fun solve(lines: List<String>, isPart2: Boolean): Long {
